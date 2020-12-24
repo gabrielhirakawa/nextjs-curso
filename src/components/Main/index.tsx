6124811372cb29499c4132e21e45e63163ce6f75
Gabriel <gabriel.souza@taki.com.vc>
2020-12-24 14:36:40 -0300

    first

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Imagem de um átomo e React Avançado escrito ao lado." />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>Typescript, ReactJS, NextJS e Styled Components</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="Desenvolvedor mexendo no computador" />
+  </S.Wrapper>
+)
+
+
+
+export default Main
