50cb9db94d13631d64dbc0c48e899f2f5cbaeedb
Gabriel <gabriel.souza@taki.com.vc>
2021-01-09 15:21:51 -0300

    ajustes storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,6 @@ module.exports = {
     //"../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   addons: [
-    '@storybook/preset-typescript',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-knobs/register'
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,13 +1,15 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
 import { text, withKnobs } from '@storybook/addon-knobs'
+
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
   decorators: [withKnobs]
-}
+} as Meta
 
-export const Basic = () => (
+export const Basic: Story = () => (
   <Main
     title={text('Title', 'React Avançado')}
     description={text(
